7a4e113c1f2362d2e2fd653dc132110bd133ce66
irwansyah1998 <nigun1998@gmail.com>
2024-10-29 20:46:41 +0700

    CRUD METHOD

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <CrudPage msg="Welcome to Your CRUD App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CrudPage from './components/CrudPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CrudPage
   }
 }
 </script>
